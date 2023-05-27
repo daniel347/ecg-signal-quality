131ac4d4f60f9c45e3a490956cab9ff14c9cdb55
daniel347 <daniel.siromani@gmail.com>
2023-05-27 13:13:43 +0100

    Made some attention maps and such, and retrained on a new feas1 data split because the last got decimated with noise.

--- Models/NoiseCNN.py
+++ Models/NoiseCNN.py
@@ -1,62 +1,66 @@
 import torch
 import torch.nn as nn
+import math
 
+# Now define a model
+hyperparameters = {"lr": 0.00075, "k1": 17, "k2": 11, "k3": 5, "k4": 3, "k5": 3, "k6": 3,
+                   "c1": 128, "c2": 256, "c3": 256, "c4": 128, "c5": 64, "c6": 32,
+                   "lstm_n_hidden": 32, "dense1": 256, "dense2": 16, "gamma": 0.5, "sched_gamma": 0.5, "sched_step": 6}
 
 class CNN(nn.Module):
 
-    def __init__(self):
+    def __init__(self, k1, k2, k3, k4, k5, k6, c1, c2, c3, c4, c5, c6, lstm_n_hidden, dense2, **_):
         super(CNN, self).__init__()
 
         self.conv_section1 = nn.Sequential(
-            nn.Conv1d(1, 128, 17, stride=4, padding=8),
+            nn.Conv1d(1, c1, k1, stride=4, padding=math.ceil((k1 - 4) / 2)),
             nn.ReLU(),
             nn.MaxPool1d(2),
-            nn.BatchNorm1d(128)
+            nn.BatchNorm1d(c1)
         )
 
         self.conv_section2 = nn.Sequential(
-            nn.Conv1d(128, 256, 11, stride=2, padding=5),
+            nn.Conv1d(c1, c2, k2, stride=2, padding=math.ceil((k1 - 2) / 2)),
             nn.ReLU(),
             nn.MaxPool1d(2),
-            nn.BatchNorm1d(256)
+            nn.BatchNorm1d(c2)
         )
 
         self.conv_section3 = nn.Sequential(
-            nn.Conv1d(256, 256, 5, padding=2),
+            nn.Conv1d(c2, c3, k3, padding="same"),
             nn.ReLU(),
             nn.MaxPool1d(2),
-            nn.BatchNorm1d(256)
+            nn.BatchNorm1d(c3)
         )
 
         self.conv_section4 = nn.Sequential(
-            nn.Conv1d(256, 128, 3, padding=1),
+            nn.Conv1d(c3, c4, k4, padding="same"),
             nn.ReLU(),
             nn.MaxPool1d(2, padding=1),
-            nn.BatchNorm1d(128)
+            nn.BatchNorm1d(c4)
         )
 
         self.conv_section5 = nn.Sequential(
-            nn.Conv1d(128, 64, 3, padding=1),
+            nn.Conv1d(c4, c5, k5, padding="same"),
             nn.ReLU(),
             nn.MaxPool1d(2, padding=1),
-            nn.BatchNorm1d(64)
+            nn.BatchNorm1d(c5)
         )
 
         self.conv_section6 = nn.Sequential(
-            nn.Conv1d(64, 32, 3, padding=1),
+            nn.Conv1d(c5, c6, k6, padding="same"),
             nn.ReLU(),
             nn.MaxPool1d(2),
-            nn.BatchNorm1d(32)
+            nn.BatchNorm1d(c6)
         )
 
-        self.lstm_n_hidden = 32
-        self.lstm = nn.LSTM(input_size=32, hidden_size=32, bidirectional=True, batch_first=True)
+        self.lstm_n_hidden = lstm_n_hidden
+        self.lstm = nn.LSTM(input_size=c6, hidden_size=lstm_n_hidden, bidirectional=True, batch_first=True)
 
-        # self.dense1 = nn.Linear(352, 128)
-        self.dense2 = nn.Linear(64, 16)
-        self.dense3 = nn.Linear(16, 1)
+        self.dense2 = nn.Linear(2 * lstm_n_hidden, dense2)
+        self.dense3 = nn.Linear(dense2, 1)
 
-        self.activation = nn.ReLU()
+        self.activation = nn.ELU()
         self.logsoftmax = nn.LogSoftmax(dim=-1)
 
         self.dropout = nn.Dropout()
@@ -90,7 +94,8 @@ class CNN(nn.Module):
         x = torch.transpose(x, 1, 2)
 
         x, (h, _) = self.lstm(x, self.init_lstm_hidden(x.shape[0], x.device))
-        x = torch.flatten(torch.transpose(h, 0, 1), 1, 2)  # torch.flatten(x, 1, -1) #
+        x = torch.flatten(torch.transpose(h, 0, 1), 1, 2)  # torch.flatten(x, 1, -1)
+        # x = torch.flatten(x, 1, -1)
 
         # [1152]
         x = self.dense2(x)
@@ -100,7 +105,6 @@ class CNN(nn.Module):
         # [128]
         x = self.dense3(x)
         # x = self.logsoftmax(x)
-        # x = torch.nn.functional.sigmoid(x)
 
         # [1]
         return x
--- Utilities/Plotting.py
+++ Utilities/Plotting.py
@@ -1,8 +1,12 @@
 from scipy import signal
 import numpy as np
+import math
 
 import matplotlib.pyplot as plt
 from matplotlib.ticker import AutoMinorLocator
+
+from sklearn.metrics import ConfusionMatrixDisplay
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
 def plot_ecg_spectrogram(x, fs=300, n_split=1, cut_range=None, figsize=(12, 5), export_quality=False):
@@ -40,12 +44,50 @@ def plot_ecg_poincare(rri, rri_len, figsize=(5, 5)):
     plt.show()
 
 
-def plot_ecg_drr(rri):
-    fig = plt.figure()
-    drri = np.diff(rri)
-    plt.plot(rri[1:], drri)
-    plt.xlabel("RR interval")
-    plt.ylabel("dRR interval")
+def plot_ecg_drr(rri, rri_len, figsize=(4, 4), export_quality=False):
+    fig, ax = plt.subplots(figsize=figsize, dpi=(250 if export_quality else 75))
+    drri = np.diff(rri[:rri_len])
+    ax.plot(rri[1:rri_len], drri, "o")
+    ax.set_xlabel("RR")
+    ax.set_ylabel("dRR")
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    fig.tight_layout()
+    plt.show()
+
+def plot_confusion_matrix(conf_mat, labels):
+    fig, ax = plt.subplots(figsize=(6, 6))
+
+    ConfusionMatrixDisplay(conf_mat, display_labels=labels).plot(cmap="GnBu", ax=ax, colorbar=False)
+    fig.tight_layout()
+    plt.show()
+
+def plot_confusion_matrix_2(confusion_matrix, labels, colour='YlOrBr'):
+    # Normalize the confusion matrix
+    normalized_matrix = confusion_matrix.astype('float') / confusion_matrix.sum(axis=1)[:, np.newaxis]
+
+    # Create a figure and axes
+    fig, ax = plt.subplots(figsize=(4, 4))
+    im = ax.imshow(normalized_matrix, cmap=colour, alpha=0.8)
+
+    # Set labels, title, and colorbar
+    ax.set_xticks(np.arange(len(labels)))
+    ax.set_yticks(np.arange(len(labels)))
+    ax.set_xticklabels(labels)
+    ax.set_yticklabels(labels)
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    # plt.colorbar(im)
+
+    # Loop over data dimensions and create text annotations
+    for i in range(len(labels)):
+        for j in range(len(labels)):
+            text = ax.text(j, i, confusion_matrix[i, j],
+                           ha="center", va="center", color="w" if normalized_matrix[i, j] > 0.5 else "k")
+
+    # Show the plot
+    fig.tight_layout()
     plt.show()
 
 
@@ -67,6 +109,7 @@ def plot_ecg(x, fs=300, n_split=1, r_peaks=None, attention=None, num_segments=No
 
         if attention is not None:
             print("Plotting attention")
+
             attention_step = (sample_len-1)/num_segments
             attention_gain = 0.5/attention.max()
             alpha = attention_gain * attention
@@ -101,5 +144,58 @@ def plot_ecg(x, fs=300, n_split=1, r_peaks=None, attention=None, num_segments=No
 
         ax[j][0].grid(which='major', linestyle='-', linewidth='0.5', color='black')
         ax[j][0].grid(which='minor', linestyle='-', linewidth='0.5', color='lightgray')
+
+    fig.tight_layout()
+
+def plot_ecg_with_attention(x, fs=300, n_split=1, r_peaks=None, attention=None, num_segments=None, figsize=(12, 8), export_quality=False, save_name=None):
+    sample_len = x.shape[0]
+    time_axis = np.arange(sample_len)/fs
+
+    cuts = np.round(np.linspace(0, sample_len-1, n_split+1)).astype(int)
+
+    y_step = 1
+
+    fig, ax = plt.subplots(n_split * 2, 1, figsize=figsize, squeeze=False, dpi=(250 if export_quality else 75))
+    for j in range(n_split):
+        ax[j*2][0].plot(time_axis[cuts[j]:cuts[j+1]], x[cuts[j]:cuts[j+1]])
+
+        if r_peaks is not None:
+            r_peaks = r_peaks[np.logical_and(r_peaks < sample_len, r_peaks >= 0)]
+            ax[j*2][0].plot(time_axis[r_peaks], x[r_peaks], "x")
+
+        if attention is not None:
+            print("Plotting attention")
+
+            att_start = int(round((attention.shape[-1]/n_split) * j))
+            att_end = int(round((attention.shape[-1]/n_split) * (j + 1)))
+
+            ax[j*2 + 1][0].imshow(attention[:, att_start:att_end], aspect="auto")
+            ax[j*2 + 1][0].set_axis_off()
+
+        ax[-2][0].set_xlabel("Time (s)")  # Only set the last axis label
+        ax[j*2][0].set_xlim((time_axis[cuts[j]], time_axis[cuts[j+1]]))
+
+        t_s = time_axis[cuts[j]]
+        t_f = time_axis[cuts[j+1]]
+
+        time_ticks = np.arange(t_s - t_s%0.2, t_f + (0.2 - t_f%0.2), 0.2)
+        # Clip the ticks to just the range, to avoid whitespace at the edges of the signal (or duplicate times)
+        time_ticks = time_ticks[np.logical_and(time_ticks > t_s, time_ticks < t_f)]
+
+        decimal_labels = ~np.isclose(time_ticks, np.round(time_ticks))
+        time_labels = np.round(time_ticks).astype(int).astype(str)
+        time_labels[decimal_labels] = ""
+
+        ax[j*2][0].set_ylim((x.min()-y_step, x.max()+y_step))
+
+        ax[j*2][0].set_xticks(time_ticks, time_labels)
+        ax[j*2][0].set_yticklabels([])
+
+
+        ax[j*2][0].xaxis.set_minor_locator(AutoMinorLocator(5))
+        ax[j*2][0].yaxis.set_minor_locator(AutoMinorLocator(5))
+
+        ax[j*2][0].grid(which='major', linestyle='-', linewidth='0.5', color='black')
+        ax[j*2][0].grid(which='minor', linestyle='-', linewidth='0.5', color='lightgray')
 
     fig.tight_layout()
